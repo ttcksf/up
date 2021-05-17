75b35ed42c210d1f0da843d7d05830e622287128
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-17 19:55:38 +0900

    index.phpにHTMLを貼り付け

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Girls Bar Andy</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="/js/wow.min.js"></script>
+    <link rel="stylesheet" href="/css/animate.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/script.js"></script>
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner inner">
+            <h1 class="header__logo"><a href="/">Girls Bar Andy</a></h1>
+            <!-- <ul class="header__nav">
+                <li><a href="" class="is-active">トップページ</a></li>
+                <li><a href="">お知らせ</a></li>
+                <li><a href="">キャスト一覧</a></li>
+                <li><a href="">店内写真</a></li>
+                <li><a href="">オンラインガールズバー</a></li>
+                <li><a href="">ログイン</a></li>
+            </ul> -->
+            <div class="drawer">
+                <input type="checkbox" class="drawer__checkbox" id="drawerCheckbox">
+                <label for="drawerCheckbox" class="drawer__icon">
+                    <span class="drawer__icon-parts"></span><!-- /.drawer__icon-parts -->
+                </label><!-- /.drawer__icon -->
+                <label for="drawerChechbox" class="drawer__overlay"></label><!-- /.drawer__overlay -->
+                <nav class="drawer__menu">
+                    <ul>
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">トップページ</a><!-- /.drawer__item-inner --></li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">お知らせ</a><!-- /.drawer__item-inner --></li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">キャスト一覧</a><!-- /.drawer__item-inner --></li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">店内写真</a><!-- /.drawer__item-inner --></li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">オンラインガールズバー</a><!-- /.drawer__item-inner --></li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">ログイン</a><!-- /.drawer__item-inner --></li><!-- /.drawer__item -->
+                    </ul>
+                </nav><!-- /.drawer__menu -->
+            </div><!-- /.drawer -->
+        </div>   
+    </header><!-- /.header -->
+
+    <div class="main-visual wow fadeIn" data-wow-duration="4s">
+            <div class="main-visual__content">
+                <div class="main-visual__image">
+                    <img src="/img/mv.png" alt="girls bar andy">
+                </div><!-- /.main-visual__image -->
+                <div class="main-visual__title">Create Night</div><!-- /.main-visual__title -->
+                <div class="main-visual__lead">
+                    素敵な出会いと落ち着きのある雰囲気で楽しい時間を提供しております。<br>
+                    Girls Bar Andyはキャスト選びからこだわり、大人の時間を楽しめる場所としてご好評を頂いております。
+                </div><!-- /.main-visual__lead -->    
+            </div><!-- /.main-visual__content -->
+    </div><!-- /.main-visual -->
+
+    <section class="online-service wow fadeIn" data-wow-duration="4s">
+        <div class="card__inner inner">
+            <div class="SPACER--80 TAB--NONE SP--NONE"></div><!-- /.SPACER--80 -->
+            <h1 class="card__head wow fadeInUp">Online Girls Bar</h1><!-- /.card__head -->
+            <h2 class="card__lead wow fadeInUp">自宅からでもたのしくお話</h2><!-- /.card__lead -->
+            <div class="SPACER--60"></div><!-- /.SPACER--50 -->
+            <div class="card__items wow fadeInUp">
+                <div class="card__item">
+                    <div class="card__number">1</div><!-- /.card__number -->
+                    <h3 class="card__title">予約日時を決定</h3><!-- /.card__title -->
+                    <p class="card__text">
+                        予約ページからご希望の日時を選択ください。<br>
+                        お一人でも対応しております。
+                    </p><!-- /.card__text -->
+                </div><!-- /.card__item -->
+                <div class="card__item">
+                    <div class="card__number">2</div><!-- /.card__number -->
+                    <h3 class="card__title">料金を前払い</h3><!-- /.card__title -->
+                    <p class="card__text">
+                        予約連絡を頂いてからメールにて決済のメールをお送りします。<br>
+                        本サービスは前払い制となっております。
+                    </p><!-- /.card__text -->
+                </div><!-- /.card__item -->
+                <div class="card__item">
+                    <div class="card__number">3</div><!-- /.card__number -->
+                    <h3 class="card__title">ビデオURLから入室</h3><!-- /.card__title -->
+                    <p class="card__text">
+                        決済が完了した段階で、ZoomのURLをメールにてお送りします。<br>
+                        Zoomのアプリは事前にお客様にてご準備ください。
+                    </p><!-- /.card__text -->
+                </div><!-- /.card__item -->
+                <div class="card__item">
+                    <div class="card__number">4</div><!-- /.card__number -->
+                    <h3 class="card__title">楽しく会話</h3><!-- /.card__title -->
+                    <p class="card__text">
+                        キャストとのお時間をお楽しみください。
+                    </p><!-- /.card__text -->
+                </div><!-- /.card__item -->
+            </div><!-- /.card__items -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <div class="card__button btn"><a href="/">ご予約はこちら</a></div><!-- /.card__button btn -->
+        </div><!-- /.card__inner inner -->
+        <div class="SPACER--80"></div><!-- /.SPACER--80 -->
+    </section><!-- /.online-service -->
+
+    <section class="shop-service wow fadeIn" data-wow-duration="4s">
+        <div class="shop-service__inner inner">
+            <div class="SPACER--80"></div><!-- /.SPACER--80 -->
+            <div class="casts">
+                <div class="casts__description">
+                    <h1 class="casts__description-head wow fadeInUp">Casts</h1><!-- /.casts__description-head -->
+                    <p class="casts__description-lead wow fadeInUp">
+                        当店のキャストをご紹介します。<br>
+                        こちらのサイトに掲載しきれていないキャストもたくさん在籍しておりますので是非ご来店ください。
+                    </p><!-- /.casts__description-lead -->
+                    <div class="casts__button btn wow fadeInUp"><a href="/">一覧をみる</a></div><!-- /.card__button btn -->
+                </div><!-- /.casts__description -->
+                <div class="casts__pickup wow fadeInUp">
+                    <img src="/img/topic.png" alt="">
+                </div><!-- /.casts__pickup-image -->
+            </div><!-- /.casts -->
+            <div class="SPACER--100 SP--NONE"></div><!-- /.SPACER--60 -->
+            <div class="shops wow fadeInUp">
+                <div class="shop"><img src="/img/shoplist1.png" alt=""></div><!-- /.shop -->
+                <div class="shop"><img src="/img/shoplist2.png" alt=""></div><!-- /.shop -->
+                <div class="shop"><img src="/img/shoplist3.png" alt=""></div><!-- /.shop -->
+                <div class="shop"><img src="/img/shoplist4.png" alt=""></div><!-- /.shop -->
+            </div><!-- /.shop__photo -->
+            <div class="SPACER--80"></div><!-- /.SPACER--80 -->
+            <div class="shops__button btn wow fadeInUp"><a href="/">店舗写真</a></div><!-- /.card__button btn -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+        </div><!-- /.shop-service__inner -->
+    </section><!-- /.casts -->
+    <section class="price wow fadeIn" data-wow-duration="4s">
+        <div class="price__inner inner">
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <h1 class="price__head wow fadeInUp">System</h1><!-- /.price__head -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <div class="price__table wow fadeInUp">
+                <table class="table">
+                    <tr>
+                        <th>13:00~19:00</th>
+                        <td>4,000円/set</td>
+                    </tr>
+                    <tr>
+                        <th>21:00~Last</th>
+                        <td>5,000円/set</td>
+                    </tr>
+                    <tr>
+                        <th>Tax</th>
+                        <td>30％</td>
+                    </tr>
+                    <tr>
+                        <th>場内指名</th>
+                        <td>2,000円</td>
+                    </tr>
+                    <tr>
+                        <th>本指名</th>
+                        <td>2,000円</td>
+                    </tr>
+                    <tr>
+                        <th>同伴</th>
+                        <td>2,000円</td>
+                    </tr>
+                </table><!-- /.table -->
+            </div><!-- /.price__content -->
+            <div class="SPACER--100"></div><!-- /.SPACER--100 -->    
+        </div><!-- /.price__inner inner -->
+    </section><!-- /.price -->
+    <section class="access wow fadeIn" data-wow-duration="4s">
+        <div class="access__inner inner">
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <h1 class="access__head">Access</h1><!-- /.access__head -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <div class="access__info">
+                <div class="access__map wow fadeInUp">
+                    <div class="iframe-wrap">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3261.4328517281906!2d136.90313111559888!3d35.17076266540431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600370d455ea0345%3A0x931fd4737c94da9a!2z44CSNDYwLTAwMDMg5oSb55-l55yM5ZCN5Y-k5bGL5biC5Lit5Yy66Yym77yT5LiB55uu77yR77yT4oiS77yR77yZIOOCouODreODs-mMpu-8og!5e0!3m2!1sja!2sjp!4v1620820328713!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div><!-- /.iframe-wrap -->
+                </div><!-- /.access__map -->
+                <div class="access__address wow fadeInUp">
+                    <p>Girls bar Andy</p>
+                    <p>〒460-0003 愛知県名古屋市中区錦3丁目13-19 アロン錦Ｂ ビル4F 東側</p>
+                    <p><a href="tel:0522658850" class="tel">TEL：0522658850</a></p>
+                    <p>営業時間：13：00～1：00</p>
+                </div><!-- /.access__address -->
+            </div><!-- /.access__info -->
+            <div class="SPACER--100"></div><!-- /.SPACER--100 -->    
+        </div><!-- /.access__inner inner -->
+    </section><!-- /.access -->
+    <footer class="footer">
+        <div class="footer__inner inner">
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <div class="footer__menu">
+                <ul class="footer__nav">
+                    <li><a href="">トップページ</a></li>
+                    <li><a href="">お知らせ</a></li>
+                    <li><a href="">キャスト一覧</a></li>
+                    <li><a href="">店内写真</a></li>
+                    <li><a href="">オンラインガールズバー</a></li>
+                    <li><a href="">ログイン</a></li>
+                </ul><!-- /.footer__nav -->
+            </div><!-- /.footer__menu -->
+            <p class="copyright">
+                Copyright &copy; 株式会社UP All Right Reserved
+            </p><!-- /.copyright -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+        </div><!-- /.footer__inner inner -->
+    </footer><!-- /.footer -->
+</body>
+</html>
