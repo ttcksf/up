28774ce666872064937596f9fa413aee98089b10
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-18 02:34:39 +0900

    front-page index archive singleのヘッダーメニューをheader-menu.phpに移動

--- archive.php
+++ archive.php
@@ -5,43 +5,7 @@
 </head>
 
 <body>
-    <header class="header">
-        <div class="header__inner inner">
-            <h1 class="header__logo"><a href="/">Girls Bar Andy</a></h1>
-            <!-- <ul class="header__nav">
-                <li><a href="" class="is-active">トップページ</a></li>
-                <li><a href="">お知らせ</a></li>
-                <li><a href="">キャスト一覧</a></li>
-                <li><a href="">店内写真</a></li>
-                <li><a href="">オンラインガールズバー</a></li>
-                <li><a href="">ログイン</a></li>
-            </ul> -->
-            <div class="drawer">
-                <input type="checkbox" class="drawer__checkbox" id="drawerCheckbox">
-                <label for="drawerCheckbox" class="drawer__icon">
-                    <span class="drawer__icon-parts"></span><!-- /.drawer__icon-parts -->
-                </label><!-- /.drawer__icon -->
-                <label for="drawerChechbox" class="drawer__overlay"></label><!-- /.drawer__overlay -->
-                <nav class="drawer__menu">
-                    <ul>
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">トップページ</a><!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">お知らせ</a><!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">キャスト一覧</a><!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">店内写真</a><!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">オンラインガールズバー</a><!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">ログイン</a><!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                    </ul>
-                </nav><!-- /.drawer__menu -->
-            </div><!-- /.drawer -->
-        </div>
-    </header><!-- /.header -->
-
+    <?php get_template_part('includes/header-menu');?>
     <section class="news">
         <div class="news__inner inner wow fadeIn" data-wow-duration="4s">
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
