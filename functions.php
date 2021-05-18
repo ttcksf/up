3a89341a1fe3a8e9410c4ada1c8f6a33ef8aed78
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-18 22:54:08 +0900

    メニュー機能を追加

--- functions.php
+++ functions.php
@@ -2,4 +2,11 @@
 add_action('init',function(){
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+
+    //menu
+    register_nav_menus([
+        'header_nav' => 'ヘッダーメニュー',
+        'drawer_nav' => 'ドロワーメニュー',
+        'footer_nav' => 'フッターメニュー',
+    ]);
 });
