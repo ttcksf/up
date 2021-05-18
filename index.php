a83244c385d034148afe6da24026ee9537a327f4
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-18 21:59:25 +0900

    archive.phpから固定ページとしてindex.phpに乗り換え　ページネーションの追加

--- index.php
+++ index.php
@@ -3,130 +3,50 @@
 <head>
     <?php get_header();?>
 </head>
+
 <body>
     <?php get_template_part('includes/header-menu');?>
-    <div class="main-visual wow fadeIn" data-wow-duration="4s">
-            <div class="main-visual__content">
-                <div class="main-visual__image">
-                    <img src="<?php echo get_template_directory_uri();?>/img/mv.png" alt="girls bar andy">
-                </div><!-- /.main-visual__image -->
-                <div class="main-visual__title">Create Night</div><!-- /.main-visual__title -->
-                <div class="main-visual__lead">
-                    素敵な出会いと落ち着きのある雰囲気で楽しい時間を提供しております。<br>
-                    Girls Bar Andyはキャスト選びからこだわり、大人の時間を楽しめる場所としてご好評を頂いております。
-                </div><!-- /.main-visual__lead -->    
-            </div><!-- /.main-visual__content -->
-    </div><!-- /.main-visual -->
-
-    <section class="online-service wow fadeIn" data-wow-duration="4s">
-        <div class="card__inner inner">
-            <div class="SPACER--80 TAB--NONE SP--NONE"></div><!-- /.SPACER--80 -->
-            <h1 class="card__head wow fadeInUp">Online Girls Bar</h1><!-- /.card__head -->
-            <h2 class="card__lead wow fadeInUp">自宅からでもたのしくお話</h2><!-- /.card__lead -->
-            <div class="SPACER--60"></div><!-- /.SPACER--50 -->
-            <div class="card__items wow fadeInUp">
-                <div class="card__item">
-                    <div class="card__number">1</div><!-- /.card__number -->
-                    <h3 class="card__title">予約日時を決定</h3><!-- /.card__title -->
-                    <p class="card__text">
-                        予約ページからご希望の日時を選択ください。<br>
-                        お一人でも対応しております。
-                    </p><!-- /.card__text -->
-                </div><!-- /.card__item -->
-                <div class="card__item">
-                    <div class="card__number">2</div><!-- /.card__number -->
-                    <h3 class="card__title">料金を前払い</h3><!-- /.card__title -->
-                    <p class="card__text">
-                        予約連絡を頂いてからメールにて決済のメールをお送りします。<br>
-                        本サービスは前払い制となっております。
-                    </p><!-- /.card__text -->
-                </div><!-- /.card__item -->
-                <div class="card__item">
-                    <div class="card__number">3</div><!-- /.card__number -->
-                    <h3 class="card__title">ビデオURLから入室</h3><!-- /.card__title -->
-                    <p class="card__text">
-                        決済が完了した段階で、ZoomのURLをメールにてお送りします。<br>
-                        Zoomのアプリは事前にお客様にてご準備ください。
-                    </p><!-- /.card__text -->
-                </div><!-- /.card__item -->
-                <div class="card__item">
-                    <div class="card__number">4</div><!-- /.card__number -->
-                    <h3 class="card__title">楽しく会話</h3><!-- /.card__title -->
-                    <p class="card__text">
-                        キャストとのお時間をお楽しみください。
-                    </p><!-- /.card__text -->
-                </div><!-- /.card__item -->
-            </div><!-- /.card__items -->
+    <section class="news">
+        <div class="news__inner inner wow fadeIn" data-wow-duration="4s">
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-            <div class="card__button btn"><a href="/">ご予約はこちら</a></div><!-- /.card__button btn -->
-        </div><!-- /.card__inner inner -->
-        <div class="SPACER--80"></div><!-- /.SPACER--80 -->
-    </section><!-- /.online-service -->
-
-    <section class="shop-service wow fadeIn" data-wow-duration="4s">
-        <div class="shop-service__inner inner">
-            <div class="SPACER--80"></div><!-- /.SPACER--80 -->
-            <div class="casts">
-                <div class="casts__description">
-                    <h1 class="casts__description-head wow fadeInUp">Casts</h1><!-- /.casts__description-head -->
-                    <p class="casts__description-lead wow fadeInUp">
-                        当店のキャストをご紹介します。<br>
-                        こちらのサイトに掲載しきれていないキャストもたくさん在籍しておりますので是非ご来店ください。
-                    </p><!-- /.casts__description-lead -->
-                    <div class="casts__button btn wow fadeInUp"><a href="/">一覧をみる</a></div><!-- /.card__button btn -->
-                </div><!-- /.casts__description -->
-                <div class="casts__pickup wow fadeInUp">
-                    <img src="<?php echo get_template_directory_uri();?>/img/topic.png" alt="">
-                </div><!-- /.casts__pickup-image -->
-            </div><!-- /.casts -->
-            <div class="SPACER--100 SP--NONE"></div><!-- /.SPACER--60 -->
-            <div class="shops wow fadeInUp">
-                <div class="shop"><img src="<?php echo get_template_directory_uri();?>/img/shoplist1.png" alt=""></div><!-- /.shop -->
-                <div class="shop"><img src="<?php echo get_template_directory_uri();?>/img/shoplist2.png" alt=""></div><!-- /.shop -->
-                <div class="shop"><img src="<?php echo get_template_directory_uri();?>/img/shoplist3.png" alt=""></div><!-- /.shop -->
-                <div class="shop"><img src="<?php echo get_template_directory_uri();?>/img/shoplist4.png" alt=""></div><!-- /.shop -->
-            </div><!-- /.shop__photo -->
-            <div class="SPACER--80"></div><!-- /.SPACER--80 -->
-            <div class="shops__button btn wow fadeInUp"><a href="/">店舗写真</a></div><!-- /.card__button btn -->
+            <h1 class="news__head">News</h1><!-- /.news__head -->
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-        </div><!-- /.shop-service__inner -->
-    </section><!-- /.casts -->
-    <section class="price wow fadeIn" data-wow-duration="4s">
-        <div class="price__inner inner">
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-            <h1 class="price__head wow fadeInUp">System</h1><!-- /.price__head -->
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-            <div class="price__table wow fadeInUp">
-                <table class="table">
-                    <tr>
-                        <th>13:00~19:00</th>
-                        <td>4,000円/set</td>
-                    </tr>
-                    <tr>
-                        <th>21:00~Last</th>
-                        <td>5,000円/set</td>
-                    </tr>
-                    <tr>
-                        <th>Tax</th>
-                        <td>30％</td>
-                    </tr>
-                    <tr>
-                        <th>場内指名</th>
-                        <td>2,000円</td>
-                    </tr>
-                    <tr>
-                        <th>本指名</th>
-                        <td>2,000円</td>
-                    </tr>
-                    <tr>
-                        <th>同伴</th>
-                        <td>2,000円</td>
-                    </tr>
-                </table><!-- /.table -->
-            </div><!-- /.price__content -->
-            <div class="SPACER--100"></div><!-- /.SPACER--100 -->    
-        </div><!-- /.price__inner inner -->
-    </section><!-- /.price -->
+            <div class="news__items">
+                <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <a href="<?php the_permalink();?>" class="news__item-link">
+                            <div class="news__item">
+                                <div class="news__img"><img src="/img/news1.png" alt=""></div><!-- /.news__img -->
+                                <h2 class="news__title"><?php the_title(); ?></h2><!-- /.news__title -->
+                                <p class="news__text">
+                                    <?php the_excerpt(); ?>
+                                </p><!-- /.news__text -->
+                            </div><!-- /.news__item -->
+                        </a><!-- /.news__item-link -->
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <p>記事が見つかりませんでした</p>
+                <?php endif; ?>
+                <div class="SPACER--80"></div><!-- /.SPACER--80 -->
+                <!-- pagenation -->
+                <?php if(paginate_links()):?>
+                <div class="pagenation">
+                    <?php
+                        echo paginate_links(array(
+                            'end_size' => 1,
+                            'mid_size' => 1,
+                            'prev_next' => true,
+                            'prev_text' => '<i class="fas fa-angle-left"></i>',
+                            'next_text' => '<i class="fas fa-angle-right"></i>',
+                        ));
+                    ?>
+                </div><!-- /pagenation -->
+                <?php endif;?>
+            </div><!-- /.news__items -->
+            <div class="SPACER--200"></div><!-- /.SPACER--100 -->
+        </div><!-- /.news__inner inner -->
+    </section><!-- /.news -->
     <?php get_footer();?>
 </body>
+
 </html>
