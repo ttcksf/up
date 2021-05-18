8cf62dafd94f63309a14d406ff8f3bbe4a885af9
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-18 14:01:10 +0900

    single.phpを作成

--- single.php
+++ single.php
@@ -1,47 +1,40 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <?php get_header();?>
+    <?php get_header(); ?>
 </head>
+
 <body>
-    <?php get_template_part('includes/header-php');?>
+    <?php get_template_part('includes/header-php'); ?>
     <section class="news__single">
         <div class="news__single__inner inner wow fadeIn" data-wow-duration="4s">
-            <div class="single">
-                <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-                <h1 class="single__head">オンラインガールズバーが開始</h1><!-- /.single__head -->
-                <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-                <div class="single__img">
-                    <img src="/img/news2.png" alt="">
-                </div><!-- /.single__img -->
-                <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-                <div class="single__text">
-                    Girls Bar Andyにて新サービスのオンラインガールズバーを開始しました。<br>
-
-                    オンラインガールズバーとは、お客様とキャストがビデオ通話にて食事や会話を楽しんでいただくサービスです。<br>
-
-                    ビデオ通話のため、基本的には同じキャストとずっと会話することができ、途中でヘルプが来たり指名が被ることがありません。<br>
-
-                    サービスの流れは以下の形です。<br>
-
-                    ①本サイトのメニューから「オンラインガールズバー」のページに遷移してください。<br>
-
-                    ②「オンラインガールズバー」のページ内で、ご希望の日程やお客様情報を入力して送信ください。<br>
-
-                    ③Eメールにてクレカによる事前決済を行います。<br>
-
-                    ④決済が完了しましたら、ZoomのURLをご連絡しますので予約時間になりましたら入室ください。<br>  
-                    
-                    ※当店ではZoomを使用しており、Zoomのインストールはあらかじめお客様のほうでお済ませください。<br>
-                </div><!-- /.single__text -->
-                <div class="SPACER--100 SP--NONE"></div><!-- /.SPACER--200 -->
-                <div class="single__button btn"><a href="/archive.html">一覧へ</a></div><!-- /.card__button btn -->
-                <div class="SPACER--200"></div><!-- /.SPACER--200 -->    
-            </div><!-- /.single -->    
-            </div><!-- /.news__single__inner -->
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <div class="single">
+                        <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+                        <h1 class="single__head"><?php the_title(); ?></h1><!-- /.single__head -->
+                        <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+                        <div class="single__img">
+                            <?php if (has_post_thumbnail()) {
+                                the_post_thumbnail();
+                            } ?>
+                        </div><!-- /.single__img -->
+                        <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+                        <div class="single__text">
+                            <?php the_content(); ?>
+                        </div><!-- /.single__text -->
+                        <div class="SPACER--100 SP--NONE"></div><!-- /.SPACER--200 -->
+                        <div class="single__button btn"><a href="/archive.html">一覧へ</a></div><!-- /.card__button btn -->
+                        <div class="SPACER--200"></div><!-- /.SPACER--200 -->
+                    </div><!-- /.single -->
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div><!-- /.news__single__inner -->
     </section><!-- /.news__single -->
 
 
-    <?php get_footer();?>
+    <?php get_footer(); ?>
 </body>
+
 </html>
