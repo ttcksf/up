2238255069665f7008837b15a38d7806a471d855
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-23 00:55:39 +0900

    titleタグの修正

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php the_title();?></title>
+    <title><?php bloginfo('name');?></title>
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/reset.css">
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/img/favicon.ico">
