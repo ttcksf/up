c8d438ca836ead7a663122b5a3819dabb7fd8159
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-23 00:56:18 +0900

    archive.phpの完成

--- archive-cast.php
+++ archive-cast.php
@@ -1,91 +1,55 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <?php get_header();?>
+    <?php get_header(); ?>
 </head>
+
 <body>
-    <?php get_template_part('includes/header-menu');?>
+    <?php get_template_part('includes/header-menu'); ?>
     <section class="casts-page">
         <div class="casts-page__inner inner wow fadeIn" data-wow-duration="4s">
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
             <div class="casts__head">Casts</div><!-- /.casts__head -->
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
             <div class="casts__photos">
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast1.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast2.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast3.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast4.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast5.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast6.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast7.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast8.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast9.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast10.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast11.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
+                <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <a href="<?php the_permalink(); ?>" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
+                            <div class="casts__photo">
+                                <?php if (has_post_thumbnail()) {
+                                    $id = get_post_thumbnail_id();
+                                    $img = wp_get_attachment_image_src($id, 'large');
+                                    echo '<img src="' . esc_url($img[0]) . '">';
+                                } ?>
+                            </div><!-- /.casts__photo -->
+                        </a><!-- /.casts__photo-link -->
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <p>キャストが登録されていません</p>
+                <?php endif; ?>
             </div><!-- /.casts__photos -->
             <div class="SPACER--80"></div><!-- /.SPACER--80 -->
-                <!-- pagenation -->
-			    <div class="pagenation">
-				    <span class="page-numbers current">1</span>
-				    <a class="page-numbers" href="#">2</a>
-				    <a class="page-numbers" href="#">3</a>
-                    <a class="page-numbers" href="#">3</a>
-                    <a class="page-numbers" href="#">3</a>
-                    <a class="page-numbers" href="#">3</a>
-                    <a class="page-numbers" href="#">3</a>
-                    <a class="page-numbers" href="#">3</a>
-				    <a class="next page-numbers" href="#"><i class="fas fa-angle-right"></i></a>
-			    </div><!-- /pagenation -->
+            <!-- pagenation -->
+            <?php if (paginate_links()) : ?>
+                <div class="pagenation">
+                    <?php
+                    echo paginate_links(array(
+                        'end_size' => 1,
+                        'mid_size' => 1,
+                        'prev_next' => true,
+                        'prev_text' => '<i class="fas fa-angle-left"></i>',
+                        'next_text' => '<i class="fas fa-angle-right"></i>',
+                    ));
+                    ?>
+                </div><!-- /pagenation -->
+            <?php endif; ?>
             <div class="SPACER--100"></div><!-- /.SPACER--100 -->
-            <div class="casts-page__button btn-large"><a href="/page-contact.html">オンラインガールズバーの予約へ</a></div><!-- /.card__button btn -->
+            <div class="casts-page__button btn-large"><a href="<?php echo esc_url(home_url('/contact'));?>">オンラインガールズバーの予約へ</a></div><!-- /.card__button btn -->
             <div class="SPACER--200"></div><!-- /.SPACER--200 -->
         </div><!-- /.casts-page__inner inner -->
     </section><!-- /.casts-page -->
 
-    <?php get_footer();?>
+    <?php get_footer(); ?>
 </body>
+
 </html>
