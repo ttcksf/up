faad0feea8ea4f13c0fcdee5486e2da0fb816a0f
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-23 16:49:32 +0900

    wp simple payの固定ページを作成

---
+++ page-failed.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <?php get_header();?>
+</head>
+<body>
+    <?php get_template_part('includes/header-menu');?>
+    <section class="404__inner inner">
+        <div class="entry-404">
+            <h1 class="entry-404-head">購入が出来ませんでした</h1><!-- /entry-404-head -->
+            <div class="entry-404-lead">購入処理が正常に行われませんでした</div><!-- /entry-404-head -->
+            <p class="entry-404-content">申し訳ありませんが、購入情報に誤りがあるかサーバー環境が混雑している可能性があります。<br>購入店舗に直接お問い合わせください。</p><!-- /entry-404-content -->
+            <div class="entry-404-btn"><a class="btn" href="/">トップページに戻る</a></div><!-- /entry-404-btn -->
+        </div><!-- /entry-404 -->
+    </section><!-- /.404__inner inner -->
+    <?php get_footer();?>
+</body>
+</html>    
---
+++ page-success.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <?php get_header();?>
+</head>
+<body>
+    <?php get_template_part('includes/header-menu');?>
+    <section class="404__inner inner">
+        <div class="entry-404">
+            <h1 class="entry-404-head">購入がありがとうございます。</h1><!-- /entry-404-head -->
+            <div class="entry-404-lead">購入が完了しました。</div><!-- /entry-404-head -->
+            <p class="entry-404-content">引き続きよろしくお願いします。<br>入力されたURLが正しいかご確認ください。</p><!-- /entry-404-content -->
+            <div class="entry-404-btn"><a class="btn" href="/">トップページに戻る</a></div><!-- /entry-404-btn -->
+        </div><!-- /entry-404 -->
+    </section><!-- /.404__inner inner -->
+    <?php get_footer();?>
+</body>
+</html>    
