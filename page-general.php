c9d439c1898f5ce4ca3dd39a19e9292bd446c58c
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-23 17:11:55 +0900

    購入ページの作成

---
+++ page-general.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <?php get_header();?>
+</head>
+<body>
+    <?php get_template_part('includes/header-menu');?>
+    <section class="404__inner inner">
+        <div class="entry-404">
+            <h1 class="entry-404-head">購入ページ</h1><!-- /entry-404-head -->
+            <div class="entry-404-lead">こちらのボタンより決済画面に移動お願い致します。</div><!-- /entry-404-head -->
+            <p class="entry-404-content">お支払いはクレジットカードのみになります。</p><!-- /entry-404-content -->
+            <div class="entry-404-btn"><?php echo do_shortcode('[simpay id="2191"]');?></div><!-- /entry-404-btn -->
+        </div><!-- /entry-404 -->
+    </section><!-- /.404__inner inner -->
+    <?php get_footer();?>
+</body>
+</html>    
