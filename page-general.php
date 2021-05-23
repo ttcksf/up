4932768301c275b943412f81f2e41edb63bff461
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-23 18:47:37 +0900

    simple stripe checkoutのプラグインを使用

--- page-general.php
+++ page-general.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <?php get_header();?>
+    <?php get_header(); ?>
 </head>
+
 <body>
-    <?php get_template_part('includes/header-menu');?>
+    <?php get_template_part('includes/header-menu'); ?>
     <section class="404__inner inner">
         <div class="entry-404">
             <h1 class="entry-404-head">購入ページ</h1><!-- /entry-404-head -->
             <div class="entry-404-lead">こちらのボタンより決済画面に移動お願い致します。</div><!-- /entry-404-head -->
             <p class="entry-404-content">お支払いはクレジットカードのみになります。</p><!-- /entry-404-content -->
-            <div class="entry-404-btn"><?php echo do_shortcode('[simpay id="2191"]');?></div><!-- /entry-404-btn -->
+            <div class="entry-404-btn"><?php echo do_shortcode('[gssc code=1]');?></div><!-- /entry-404-btn -->
         </div><!-- /entry-404 -->
     </section><!-- /.404__inner inner -->
-    <?php get_footer();?>
+    <?php get_footer(); ?>
 </body>
-</html>    
+
+</html>
--- page-midnight.php
+++ page-midnight.php
@@ -10,7 +10,7 @@
             <h1 class="entry-404-head">購入ページ</h1><!-- /entry-404-head -->
             <div class="entry-404-lead">こちらのボタンより決済画面に移動お願い致します。</div><!-- /entry-404-head -->
             <p class="entry-404-content">お支払いはクレジットカードのみになります。</p><!-- /entry-404-content -->
-            <div class="entry-404-btn"><?php echo do_shortcode('[simpay id="2193"]');?></div><!-- /entry-404-btn -->
+            <div class="entry-404-btn"><?php echo do_shortcode('[gssc code=2]');?></div><!-- /entry-404-btn -->
         </div><!-- /entry-404 -->
     </section><!-- /.404__inner inner -->
     <?php get_footer();?>
