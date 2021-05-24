471aca5eb79f1a81e9d9089b6292cc104e83a966
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-05-24 09:54:43 +0900

    single.phpの画像の表示方法を修正

--- single.php
+++ single.php
@@ -15,19 +15,19 @@
                         <div class="SPACER--60"></div><!-- /.SPACER--60 -->
                         <h1 class="single__head"><?php the_title(); ?></h1><!-- /.single__head -->
                         <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-                        <div class="single__img">
-                            <?php if (has_post_thumbnail()) {
-                                $id = get_post_thumbnail_id();
-                                $img = wp_get_attachment_image_src($id);
-                                echo '<img src="' . esc_url($img[0]) . '">';
-                            } ?>
-                        </div><!-- /.single__img -->
+
+                        <?php if (has_post_thumbnail()) {
+                            $id = get_post_thumbnail_id();
+                            $img = wp_get_attachment_image_src($id);
+                            echo  '<div class="single__img"><img src="' . esc_url($img[0]) . '"></div>';
+                        }?>
+
                         <div class="SPACER--60"></div><!-- /.SPACER--60 -->
                         <div class="single__text">
                             <?php the_content(); ?>
                         </div><!-- /.single__text -->
                         <div class="SPACER--100 SP--NONE"></div><!-- /.SPACER--200 -->
-                        <div class="single__button btn"><a href="<?php echo esc_url(home_url('/news'));?>">一覧へ</a></div><!-- /.card__button btn -->
+                        <div class="single__button btn"><a href="<?php echo esc_url(home_url('/news')); ?>">一覧へ</a></div><!-- /.card__button btn -->
                         <div class="SPACER--200"></div><!-- /.SPACER--200 -->
                     </div><!-- /.single -->
                 <?php endwhile; ?>
