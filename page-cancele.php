aadef2d2fdfd62652f61eac94e81e4fdddc50b7f
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-06 15:52:33 +0900

    トップページへのリンクを追加

--- page-cancele.php
+++ page-cancele.php
@@ -10,7 +10,7 @@
             <h1 class="entry-404-head">キャンセルが行われました</h1><!-- /entry-404-head -->
             <div class="entry-404-lead">お申込み内容のキャンセルが実行されました</div><!-- /entry-404-head -->
             <p class="entry-404-content">またのご来店をお待ちしております。</p><!-- /entry-404-content -->
-            <div class="entry-404-btn"><a class="btn" href="/">トップページに戻る</a></div><!-- /entry-404-btn -->
+            <div class="entry-404-btn"><a class="btn" href="<?php echo esc_url(home_url('/')); ?>">トップページに戻る</a></div><!-- /entry-404-btn -->
         </div><!-- /entry-404 -->
     </section><!-- /.404__inner inner -->
     <?php get_footer();?>
--- page-success.php
+++ page-success.php
@@ -10,7 +10,7 @@
             <h1 class="entry-404-head">購入がありがとうございます。</h1><!-- /entry-404-head -->
             <div class="entry-404-lead">購入が完了しました。</div><!-- /entry-404-head -->
             <p class="entry-404-content">引き続きよろしくお願いします。<br>入力されたURLが正しいかご確認ください。</p><!-- /entry-404-content -->
-            <div class="entry-404-btn"><a class="btn" href="/">トップページに戻る</a></div><!-- /entry-404-btn -->
+            <div class="entry-404-btn"><a class="btn" href="<?php echo esc_url(home_url('/')); ?>">トップページに戻る</a></div><!-- /entry-404-btn -->
         </div><!-- /entry-404 -->
     </section><!-- /.404__inner inner -->
     <?php get_footer();?>
